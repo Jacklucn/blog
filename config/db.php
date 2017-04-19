7dbe2a9b7ba2b720ace379db00e2af70cb4a57d7
Jacklucn <jacklucn@icloud.com>
2017-04-19 14:32:15 +0800

    init
    init

---
+++ config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=jacklu',
+    'username' => 'root',
+    'password' => '',
+    'charset' => 'utf8',
+];
