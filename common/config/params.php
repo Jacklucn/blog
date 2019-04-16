58d12d27eabce74777f1cd38e83bae11963e5393
Jacklu <jacklu.net@gmail.com>
2019-04-16 15:43:12 +0800

    修改上传封面图

--- common/config/params.php
+++ common/config/params.php
@@ -3,5 +3,4 @@ return [
     'adminEmail' => 'admin@example.com',
     'supportEmail' => 'support@example.com',
     'user.passwordResetTokenExpire' => 3600,
-    'assetDomain' => 'http://jacklucn.local.com/frontend/web/uploads' // 本地上传的图片访问路径前缀
 ];
